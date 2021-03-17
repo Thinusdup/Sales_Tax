55bd5251529d942b07c603a47770374ccdb8689c
Thinus <Thinusdupisanie@gmail.com>
2021-03-17 10:05:58 +0200

    Add Test Project and dependencies

---
+++ Sales_Tax/Sales_Tax_Tests/SalesTaxManagerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Sales_Tax_Tests
+{
+    public class SalesTaxManagerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
