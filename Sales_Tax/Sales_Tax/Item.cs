fa81fcf5cf169ed54f924d33a40063ea458f8e8f
Thinus <Thinusdupisanie@gmail.com>
2021-03-17 09:14:56 +0200

    Add Item Class

---
+++ Sales_Tax/Sales_Tax/Item.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sales_Tax
+{
+   public class Item
+    {
+        public int Quantity { get; set; }
+        public  string Name { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
