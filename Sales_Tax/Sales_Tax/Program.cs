edd77ac01138af77e87130b2cd4f877838576490
Thinus <Thinusdupisanie@gmail.com>
2021-03-17 09:09:36 +0200

    Add empty console project

---
+++ Sales_Tax/Sales_Tax/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sales_Tax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
